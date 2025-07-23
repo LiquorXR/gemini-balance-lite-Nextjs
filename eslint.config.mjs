f5d67d73b9e14929333d7273e015f04b65672c1d
Arui <122691207+LiquorXR@users.noreply.github.com>
2025-07-24 00:35:46 +0800

    011

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname
+});
+
+export default [
+    ...compat.extends("next/core-web-vitals")
+];
